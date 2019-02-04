929d77dd69ecec47a1ad93e6e89b3cb929bb8c86
rebirthbtn24 <rebirthbtn@gmail.com>
2019-02-04 22:46:34 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 増田弘毅 on 2019/02/04.
+//  Copyright © 2019 rebirthbtn24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
